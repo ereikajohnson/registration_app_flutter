2c1c34ed375a66d1d71e41a33221b79eea1a688a
ereikajohnson <ereikajohnson@gmail.com>
2025-07-21 13:10:43 +0530

    firebase connected

--- lib/login.dart
+++ lib/login.dart
@@ -1,88 +1,111 @@
 import 'package:flutter/material.dart';
+import 'package:registration_app_flutter/signup.dart';
 
-class Loginpage extends StatelessWidget {
+class Loginpage extends StatefulWidget {
   const Loginpage({super.key});
 
+  @override
+  State<Loginpage> createState() => _LoginpageState();
+}
+
+class _LoginpageState extends State<Loginpage> {
+  TextEditingController emailcontroller=TextEditingController();
+  TextEditingController passcontroller=TextEditingController();
+  final formkey=GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Center(
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Column(mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(height: 40),
-              Text(
-                "Login",
-                style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
-              ),
-              SizedBox(height: 20),
-              Text(
-                "Login to your account",
-                style: TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.grey,
+      body: Form(key: formkey,
+        child: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(mainAxisSize: MainAxisSize.min,
+              children: [
+                SizedBox(height: 40),
+                Text(
+                  "Login",
+                  style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
                 ),
-              ),
-              SizedBox(height: 40),
-              Align(alignment: Alignment.topLeft, child: Text("Email")),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
+                SizedBox(height: 20),
+                Text(
+                  "Login to your account",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.grey,
                   ),
                 ),
-              ),
-              SizedBox(height: 40),
-              Align(alignment: Alignment.topLeft, child: Text("Password")),
-              TextField(
-                keyboardType: TextInputType.visiblePassword,
-                decoration: InputDecoration(
-                  fillColor: Colors.white,
-                  filled: true,
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(12),
-                  ),
-                ),
-              ),
-              SizedBox(height: 40),
-              SizedBox(
-                height: 50,
-                width: 300,
-                child: ElevatedButton(
-                  style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.blue,
-                    foregroundColor: Colors.white,
-                    side: BorderSide(color: Colors.black),
-                  ),
-                  onPressed: () {},
-                  child: Text("Login"),
-                ),
-              ),
-              SizedBox(height: 10,),
-               Row(mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text(
-                      "Don't have an account?",
-                      style: TextStyle(color: Colors.black),
+                SizedBox(height: 40),
+                Align(alignment: Alignment.topLeft, child: Text("Email")),
+                TextFormField(controller: emailcontroller,validator: (value) {
+                  if(value ==null||value.isEmpty){return "email is required ";}
+                  if (!value.contains("@")){
+                    return "@ is required";
+                  }
+                },
+                
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                    Text(
-                      "SignUp",
-                      style: TextStyle(
-                        color: Colors.black,
-                        fontWeight: FontWeight.bold,
-                      ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                Align(alignment: Alignment.topLeft, child: Text("Password")),
+                TextFormField(controller: passcontroller,validator: (value) {
+                  if (value==null||value.isEmpty) {return "password is required";}
+                },
+                  keyboardType: TextInputType.visiblePassword,
+                  decoration: InputDecoration(
+                    fillColor: Colors.white,
+                    filled: true,
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(12),
                     ),
-                  ],
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  height: 50,
+                  width: 300,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                      foregroundColor: Colors.white,
+                      side: BorderSide(color: Colors.black),
+                    ),
+                    onPressed: () {},
+                    child: Text("Login"),
+                  ),
                 ),
                 SizedBox(height: 10,),
-              Image(image: AssetImage("assets/ere.jpg")),
-            ],
+                 Row(mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Don't have an account?",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                      TextButton(onPressed: () {
+                        Navigator.push(context, MaterialPageRoute(builder: (context) => Signuppage(),));
+                      },
+                        child: Text(
+                          "SignUp",
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 10,),
+                Image(image: AssetImage("assets/image2.jpg"),height: 200,width: 200),
+              ],
+            ),
           ),
         ),
       ),
